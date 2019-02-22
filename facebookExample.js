ca5f680edc7f826bf604159167e3499cac21bf2e
Anurag Hakeem <anuhakee@publicisgroupe.net>
2019-02-22 15:37:28 +0530

    Day9_Part2

--- facebookExample.js
+++ facebookExample.js
@@ -1,12 +1,14 @@
-var myPost = [{ text: "", time: "" }]
+var a = document.getElementById("onc");
+a.addEventListener("click", createPost);
 
+var myPost = [{ text: "", time: "" }]
 function createPost() {
     var postContent = document.getElementById("myPost");
     var postContainer = document.getElementById("postContainer");
     postContainer.innerHTML = "";
-    myPost.push({ text: posteContent.value, time: new Date() });
+    myPost.push({ text: postContent.value, time: new Date() });
 
-    for (var i = myPost.length-1; i >= 0; i--) {
+    for (var i = myPost.length - 1; i >= 0; i--) {
 
         var myCard = `<div class="card">
                     <div class="card-body">

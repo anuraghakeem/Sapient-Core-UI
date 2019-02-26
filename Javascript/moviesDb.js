0b470a2dc8e0d7eaa99cfaf1be01bb7a1f5c6c8d
anuraghakeem <anuraghakeem@gmail.com>
2019-02-27 00:01:20 +0530

    Home

---
+++ Javascript/moviesDb.js
@@ -0,0 +1,25 @@
+(function () {
+    // body of the function
+    'use strict';
+
+    alert("Check Console");
+
+    var movies = [{ title: "In Bruges", rating: 5, hasWatched: true },
+    { title: "Sholay", rating: 3.5, hasWatched: false }
+    ];
+
+    movies.forEach(function (movie) {
+        if (movie.hasWatched) {
+            var sen = "You have watched " + movie.title + " - " + movie.rating + " stars";
+            console.log(sen);
+        }else{
+            var sen = "You have not watched " + movie.title + " - " + movie.rating + " stars";
+            console.log(sen);
+        }
+    });
+
+
+
+}());
+
+
